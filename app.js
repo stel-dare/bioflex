11f8f689da34618da0ef1731bb295dace71f895a
stelladare2@gmail.com <stelladare2@gmail.com>
2020-03-11 17:58:27 +0000

    Home section: images chosen ,yet to be reduced

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 app = express();
 
+app.use(express.static('public'));
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
 app.listen(3000,()=>{
     console.log('Listening on port 3000');
 });
