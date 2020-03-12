44aedb7bf9ef8c9a651052e4454ab4da6b022828
stelladare2@gmail.com <stelladare2@gmail.com>
2020-03-12 19:06:10 +0000

    Deploy to heroku

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.use(express.static('public'));
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/index.html');
 });
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log('Listening on port 3000');
 });
